3a831c28b0824a0700501b0c66179bc85148ab48
= <alexandressoares383@gmail.com>
2025-10-01 17:48:14 -0300

    feat: criando as entidades

--- src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
+++ src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b9a41bc8c8863e466659e542b69447b5d04409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edec93960415a05dd72b14f95709609d9b4793ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
