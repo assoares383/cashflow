681426c584d0efad57b1d945020e50dda78ad934
= <alexandressoares383@gmail.com>
2025-10-01 18:52:50 -0300

    feat: criando um database context

--- src/CashFlow.Communication/obj/Debug/net8.0/CashFlow.Communication.AssemblyInfo.cs
+++ src/CashFlow.Communication/obj/Debug/net8.0/CashFlow.Communication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edec93960415a05dd72b14f95709609d9b4793ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a831c28b0824a0700501b0c66179bc85148ab48")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense>  Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflow_db;Uid=cashflow_admin;Pwd=cashflow#root;";
+
+        var version = new Version(8, 0, 27);
+        var serverVersion = new MySqlServerVersion(version);
+        
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
