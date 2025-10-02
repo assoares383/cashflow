1479bf4b242852e7bd9ea2daa7406fd352046576
= <alexandressoares383@gmail.com>
2025-10-02 19:55:41 -0300

    feat: utilizando appsettings para a Connection Strings

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,15 +5,6 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options): base(options) {}
     public DbSet<Expense>  Expenses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflow_db;Uid=cashflow_admin;Pwd=cashflow#root;";
-
-        var version = new Version(8, 0, 27);
-        var serverVersion = new MySqlServerVersion(version);
-        
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
 }
